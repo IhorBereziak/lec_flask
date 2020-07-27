87dd19907a21c6ed628e4c80131ad714ab6210aa
Ihor <berezjak@i.ua>
2020-07-28 00:46:36 +0300

    add edit

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
-from wtforms import Form, StringField
+from wtforms import Form, StringField, SelectField
+from app.posts.models import Tag
 
+tags = [(tag.name, tag.name) for tag in Tag.query.all()]
 class Posts_Form(Form):
     title = StringField('Title')
     text = StringField('Text')
+    tag = SelectField('Tag', choices=tags)
+
+class Tag_Form(Form):
+    name = StringField('Name')
 
 
--- app/posts/vievs.py
+++ app/posts/vievs.py
@@ -1,14 +1,16 @@
 from flask import Blueprint, render_template, request
-from app.forms import Posts_Form
+from app.forms import Posts_Form, Tag_Form
 from app import db
 
 posts = Blueprint('posts', __name__, template_folder='templates', static_folder='static')
 
-from app.models import Post
+from app.posts.models import Post, Tag
+
 
 @posts.route('/')
 def index():
     return render_template('posts/index.html')
+
 
 @posts.route('/new/', methods=['POST', 'GET'])
 def new_posts():
@@ -26,13 +28,48 @@ def new_posts():
     if request.method == 'POST':
         title = request.form['title']
         text = request.form['text']
+        tag_name = request.form['tag']
+
+        tag = Tag.query.filter_by(name=tag_name).first()
         post = Post(title=title, text=text)
+
+        post.tag.append(tag)
         db.session.add(post)
         db.session.commit()
     return render_template('posts/new_post.html', name=title, mail=text, form=form)
+
 
 @posts.route('/all/')
 def all_posts():
     posts = Post.query.all()
     return render_template('posts/all.html', posts=posts)
 
+
+@posts.route('/new-tag/', methods=['POST', 'GET'])
+def new_tag():
+    form = Tag_Form()
+    if request.method == 'POST':
+        name = request.form['name']
+        tag = Tag(name=name)
+        db.session.add(tag)
+        db.session.commit()
+    tags = Tag.query.all()
+    return render_template('posts/new_tag.html', form=form, tags=tags)
+
+
+@posts.route('/edit/<post_name>', methods=['POST', 'GET'])
+def edit_post(post_name):
+    post = Post.query.filter_by(title=post_name).first()
+    form = Posts_Form(title=post.title, text=post.text)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.text = request.form['text']
+        tag_name = request.form['tag']
+
+        tag = Tag.query.filter_by(name=tag_name).first()
+
+        post.tag.append(tag)
+        db.session.add(post)
+        db.session.commit()
+        post = Post.query.filter_by(title=post_name).first()
+    return render_template('posts/edit_post.html', form=form, post=post)
